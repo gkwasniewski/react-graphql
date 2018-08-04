8f0602de9f8676a9234de79625da74ea520f63ec
Grzegorz Kwaśniewski <gkw@corcode.com>
2018-08-04 22:13:57 +0200

    add sorting table method v1

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper';
+import _ from 'lodash';
 
 import Product from "./Product";
+import './ProductList.css';
 
 const GQL_QUERY = gql`{
     products: allProducts(count: 25) {
@@ -16,45 +16,65 @@ const GQL_QUERY = gql`{
 `
 
 class ProductList extends Component {
-  render() {
-    return (
-        <Query query={GQL_QUERY}>
-         {({ loading, error, data }) => {
-          if (loading) return <div>Fetching</div>
-          if (error) return <div>Error</div>
-    
-          const productsToRender = data.products;
-    
-          return (
-            <div>
-                <Paper>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Product ID</TableCell>
-                                <TableCell>Product Name</TableCell>
-                                <TableCell numeric>Price</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                         {productsToRender.map(product => {
-                             return(
-                                <TableRow key={product.id}>
-                                    <TableCell>{product.id}</TableCell>
-                                    <TableCell>{product.name}</TableCell>
-                                    <TableCell>{product.price}</TableCell>
-                                </TableRow>
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [],
+        }
+    }
+
+    onSort = (column, dataToSort) => (e) => {     
+        if (column === 'productName') {
+            this.setState({data: _.orderBy(dataToSort, ['name'], ['asc'])})
+        } else {
+            this.setState({data: _.orderBy(dataToSort, ['price'], ['asc'])})
+        }
+    };
+
+    render() {
+        return (
+            <Query query={GQL_QUERY}>
+                {({ loading, error, data }) => {
+                if (loading) return <div>Fetching</div>
+                if (error) return <div>Error</div>
+                
+                let productsToRender = this.state.data;
+
+                if(this.state.data.length === 0 ) {
+                    productsToRender = data.products;
+                }
+               
+                return (
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th onClick={this.onSort('productName', productsToRender)}>
+                                        Product Name
+                                    </th>
+                                    <th onClick={this.onSort('productPrice', productsToRender)}>
+                                        Product Price
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {productsToRender.map(product => {
+                                return(
+                                    <tr key={product.id} className="product-row">
+                                        <td>{product.name}</td>
+                                        <td>{product.price}</td>
+                                        <td>{product.id}</td>
+                                    </tr>
                                 )
-                            })};
-                        </TableBody>
-                    </Table>
-                </Paper>
-            </div>
-          )
-        }}
-      </Query>
-    )
-  }
+                            })}
+                            </tbody>
+                        </table>
+                    </div>
+                )
+            }}
+         </Query>
+        )
+    }
 }
 
 export default ProductList
