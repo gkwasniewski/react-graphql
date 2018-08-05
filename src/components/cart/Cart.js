335e3f4d68fd4cb44e7409474c4b2e587df6cbd1
Grzegorz Kwaśniewski <gkw@corcode.com>
2018-08-05 17:25:58 +0200

    add total section

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import Paper from '@material-ui/core/Paper';
+import _ from 'lodash';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 import './Cart.css';
 import CartItem from './cartItem/CartItem';
@@ -7,15 +11,27 @@ import CartItem from './cartItem/CartItem';
 class Cart extends Component {
     
     render() {
-        console.log(this.props.products)
-        
+
         const productItem = this.props.products.map(element => {
             return <CartItem id={element.id} name={element.name} price={element.price} key={element.id}></CartItem>
         });
 
+        const sumProductsPrice = _.sumBy(this.props.products, (o) => { return parseFloat(o.price); })
+
         return (     
             <div>
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="title" color="inherit">
+                            Photos
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
                 <Paper className="cart-container">{productItem}</Paper>
+                <Paper className="cart-total">
+                    <p>Total items: {this.props.products.length}</p>
+                    <p>Total price: {sumProductsPrice}</p>
+                </Paper>
             </div>
         )
     }
